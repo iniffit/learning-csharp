dababc84e25bbc1cecc65078fd5cb7212a3e8b69
Tiffini <tiffini.baldwin@gmail.com>
2020-08-29 19:15:19 -0400

    Reviewed If statements and Loops
    Wrote some code to review if/else logic. Also went over while, do, and for loops.

--- branches-tutorial/Program.cs
+++ branches-tutorial/Program.cs
@@ -4,9 +4,94 @@ namespace BranchesAndLoops
 {
     class Program
     {
+        static void ExploreIf()
+        {
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10.");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+
+            int c = 4;
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+
+            if ((a + b + c > 10) || (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not equal to the second");
+            }
+        }
+
+        static void WhileDoForLoops()
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+
+            for (int index = 0; index < 10; index++)
+            {
+                Console.WriteLine($"Hello World! The index is {index}");
+            }
+
+            for (int row = 1; row < 11; row++)
+            {
+                Console.WriteLine($"The row is {row}");
+            }
+
+            for (char column = 'a'; column < 'k'; column++)
+            {
+                Console.WriteLine($"The column is {column}");
+            }
+
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }            
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ExploreIf();
+
+            // WhileDoForLoops();
+
+        // This is the answer to the challenge question. I looked for it in the documentation
+        // I could not even figure out where to start on my own.
+        // After typing it out and talking it through, it makes sense now
+            int sum = 0;
+            for (int number = 1; number < 21; number++)
+            {
+                if (number % 3 == 0)
+                {
+                    sum = sum + number;
+                }
+            }
+            Console.WriteLine($"The sum is  {sum}");
         }
     }
 }
