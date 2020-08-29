0802460d78d5683d57f7dfc81aa2547880aa81c3
Tiffini <tiffini.baldwin@gmail.com>
2020-08-29 17:23:45 -0400

    Added new methods
    Added methods for testing doubles, decimals, and finding the area of a circle that has a radius of 2.5

--- numbers-quickstart/Program.cs
+++ numbers-quickstart/Program.cs
@@ -50,12 +50,8 @@ namespace NumbersInCSharp
             Console.WriteLine(h);
         }
 
-        static void Main(string[] args)
+        static void TestLimits()
         {
-            // WorkingWithIntegers();
-
-            // OrderPrecedence();
-
             int a = 7;
             int b = 4;
             int c = 3;
@@ -68,6 +64,72 @@ namespace NumbersInCSharp
             int min = int.MinValue;
             Console.WriteLine($"The range of integers is {min} to {max}");
 
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+        }
+
+        static void WorkWithDoubles()
+        {
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a + b) / c;
+            Console.WriteLine(d);
+
+            double e = 19;
+            double f = 23;
+            double g = 8;
+            double h = (e - f) / g;
+            Console.WriteLine(h);
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double id {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+
+            double eigth = 1.0 / 8.0;
+            Console.WriteLine(eigth);
+
+            double justWhatevs = 45.0 / 982 + 9875 * 563 / 234;
+            Console.WriteLine(justWhatevs);            
+        }
+
+        static void WorkWithDecimals()
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);            
+        }
+        static void AreaOfCircle()
+        {
+            double radius = 2.50;
+            double area = (radius * radius) * Math.PI;
+            Console.WriteLine(area);
+
+        }        
+        static void Main(string[] args)
+        {
+            WorkingWithIntegers();
+
+            OrderPrecedence();
+
+            TestLimits();
+
+            WorkWithDoubles();
+
+            WorkWithDecimals();
+
+            AreaOfCircle();
         }
     }
 }
