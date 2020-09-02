5682d8a78a235fdbdb993fbe826f34cc5c588e84
Tiffini <tiffini.baldwin@gmail.com>
2020-09-02 19:33:52 -0400

    Added data member to BankAccount
    Added a private, static data member to address the creation of account numbers for each new bank account.

--- classes/BankAccount.cs
+++ classes/BankAccount.cs
@@ -6,8 +6,13 @@ namespace classes
     //public class BankAccount defines the class or type you're creating
     public class BankAccount
     {
-        // public strings, decimal, voids are five members of the BankAccount class
-        // these first three are properties - code validates/enforces rules
+        // data member that kicks off the account number sequence. 
+        // it's private, so only code inside this class can access it
+        // it's static, so it's shared by all BankAccount objects
+        private static int accountNumberSeed = 1234567890;
+
+        // public strings, decimal, voids are five members of the BankAccount
+        // class these first three are properties - code validates/enforces rules
         public string Number { get; }
         public string Owner { get; set; }
         public decimal Balance { get; }
@@ -18,6 +23,9 @@ namespace classes
         {
             this.Owner = name;
             this.Balance = intialBalance;
+            // incremints account number up by one for each account created.
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
         }
         // these two are methods - blocks of code that perform a single function
         public void MakeDeposit(decimal amount, DateTime date, string note)
