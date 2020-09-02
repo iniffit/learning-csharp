7f5b78f28bd1ad389ac3228e756171dc581566fc
Tiffini <tiffini.baldwin@gmail.com>
2020-09-01 20:52:15 -0400

    Updating the list
    Adding and removing names from the list.

--- list-tutorial/Program.cs
+++ list-tutorial/Program.cs
@@ -12,6 +12,15 @@ namespace list_tutorial
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
         }
     }
 }
