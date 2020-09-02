28622cd5eb4125ed307400c826d51792b2e64fee
Tiffini <tiffini.baldwin@gmail.com>
2020-09-01 20:46:50 -0400

    List collections
    Learning to manage data collections using the generic list type

---
+++ list-tutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> {"Tiffini", "Ana", "Felipe"};
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+        }
+    }
+}
