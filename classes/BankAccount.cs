88534dc9b2ded02472a94f4973d0fbf00848e5b1
Tiffini <tiffini.baldwin@gmail.com>
2020-09-04 10:42:00 -0400

    Updates to log transactions, there's a bug
    There's a bug in the constructor in BankAccounts.cs that I'm having some trouble working through. I've followed the Microsoft guide to build this, so I will consult it again later.

--- classes/BankAccount.cs
+++ classes/BankAccount.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 // namespace provides a way to logically organize code
 namespace classes
@@ -15,24 +16,59 @@ namespace classes
         // class these first three are properties - code validates/enforces rules
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
 
         // this is a constructor - a member used to initialize objects of the class type.
         // has same name as the class.
         public BankAccount(string name, decimal intialBalance)
         {
             this.Owner = name;
+            MakeDeposit(intialBalance, DateTime.Now, "Initial Balance");
+
             this.Balance = intialBalance;
+            
             // incremints account number up by one for each account created.
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
         }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+        
+        
         // these two are methods - blocks of code that perform a single function
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient fund for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
     }
 }
--- classes/Program.cs
+++ classes/Program.cs
@@ -8,7 +8,21 @@ namespace classes
         {
             var account = new BankAccount("Tiffini", 1000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
-            
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance.");
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
